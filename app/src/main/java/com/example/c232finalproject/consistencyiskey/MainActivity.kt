6bac8f3be1d9ea169abe393b6964865c78d235b2
Henry Orsborn <henryorsborn@gmail.com>
2021-08-01 23:58:04 -0400

    Base application created

--- app/src/main/java/com/example/c232finalproject/consistencyiskey/MainActivity.kt
+++ app/src/main/java/com/example/c232finalproject/consistencyiskey/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.c232finalproject.consistencyiskey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val days: MutableList<Int> = mutableListOf()
+        for(i in 0..365) {
+            days.add(i)
+        }
+        val statuses: MutableList<Boolean> = mutableListOf()
+        for(i in 0..365) {
+            statuses.add(true)
+        }
+        findViewById<RecyclerView>(R.id.main_recycler).adapter = MainRecyclerViewAdapter(days.toList(), statuses.toList(),this)
     }
 }
---
+++ app/src/main/java/com/example/c232finalproject/consistencyiskey/MainRecyclerViewAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.c232finalproject.consistencyiskey
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainRecyclerViewAdapter(
+    private val days: List<Int>,
+    private val statuses: List<Boolean>,
+    private val context: Context
+) : RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val dayLabel: TextView = view.findViewById(R.id.day_label)
+        val imageView: ImageView = view.findViewById(R.id.image_view)
+        val rootView: LinearLayout = view as LinearLayout
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.day_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        ("Day ${days[position]}").also { viewHolder.dayLabel.text = it }
+        if(!statuses[position]) {
+            viewHolder.imageView.visibility = View.INVISIBLE
+        }
+        viewHolder.rootView.setOnClickListener {
+            val i: Intent = Intent(context, TodoActivity::class.java).apply {
+                putExtra("key", days[position])
+            }
+            context.startActivity(i)
+        }
+    }
+
+    override fun getItemCount() = days.size
+
+}
