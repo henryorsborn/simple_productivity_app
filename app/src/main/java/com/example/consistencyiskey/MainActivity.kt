0440b313db52280ecf5b6d19aa3a48baf586f92e
Henry Orsborn <henryorsborn@gmail.com>
2021-08-18 22:28:45 -0400

    fixed imports again

--- app/src/main/java/com/example/consistencyiskey/MainActivity.kt
+++ app/src/main/java/com/example/consistencyiskey/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.c232finalproject.consistencyiskey.MainRecyclerViewAdapter
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.lang.Exception
 import java.io.*
